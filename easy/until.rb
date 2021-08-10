5de757b2e7ff9efaf3e30c722d5f95b2f2dc2bff
Epic91 <hernandeze791@gmail.com>
2021-08-10 00:29:09 -0500

    until challenge ruby

---
+++ easy/until.rb
@@ -0,0 +1,5 @@
+# Call the method coder.practice until coder.oh_one? becomes true. Use the until control structure. until is the logical equivalent of while not.
+
+until coder.oh_one?
+    coder.practice
+end
