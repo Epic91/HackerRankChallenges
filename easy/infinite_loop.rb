c50b84beaa77d1b954a2ecdeaab47bd2501ca7fc
Epic91 <hernandeze791@gmail.com>
2021-08-10 00:26:20 -0500

    infinite loop ruby

---
+++ easy/infinite_loop.rb
@@ -0,0 +1,5 @@
+# Use an infinite loop and call the method coder.practice within it and break if coder.oh_one? is true. break if conditions in Ruby are of the form
+
+loop do coder.practice 
+    break if coder.oh_one?
+end
